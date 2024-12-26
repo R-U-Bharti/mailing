e6260092ffa43ac02340ecd8930b44db5bf07b57
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2024-12-26 09:57:51 +0530

    Updates

---
+++ assets/ProtectedRoutes-Q1MS317T.js
@@ -0,0 +1,1 @@
+import{j as t,O as o,N as s}from"./index-DacGb_Ge.js";const r=()=>{const e=localStorage.getItem("token");return t.jsx(t.Fragment,{children:e?t.jsx(o,{}):t.jsx(s,{to:"/"})})};export{r as default};
