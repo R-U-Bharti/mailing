1571e66fb69650017936fc87d532c96c165d4f3f
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2024-12-26 10:13:13 +0530

    Updates

---
+++ assets/ProtectedRoutes-Bv6GaS1s.js
@@ -0,0 +1,1 @@
+import{j as t,O as o,N as s}from"./index-53ge1A5h.js";const r=()=>{const e=localStorage.getItem("token");return t.jsx(t.Fragment,{children:e?t.jsx(o,{}):t.jsx(s,{to:"/"})})};export{r as default};
